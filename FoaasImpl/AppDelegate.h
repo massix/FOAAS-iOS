46d693365710a739077fdbd5fe94e0c5e30ca2fe
Massimo Gengarelli <massimo.gengarelli@gmail.com>
2014-06-25 19:19:33 +0200

    Initial Commit

---
+++ FoaasImpl/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  FoaasImpl
+//
+//  Created by Massimo Gengarelli on 25/06/14.
+//  Copyright (c) 2014 Massimo Gengarelli. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ FoaasImplTests/FoaasImplTests.m
@@ -0,0 +1,34 @@
+//
+//  FoaasImplTests.m
+//  FoaasImplTests
+//
+//  Created by Massimo Gengarelli on 25/06/14.
+//  Copyright (c) 2014 Massimo Gengarelli. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface FoaasImplTests : XCTestCase
+
+@end
+
+@implementation FoaasImplTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
